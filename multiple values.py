f35108b7ce460d02fb5f29fec72745d9c42b1d06
PhoniciaAnne <153049020+PhoniciaAnne@users.noreply.github.com>
2023-12-19 22:34:13 +0530

    Update multiple values.py

--- multiple values.py
+++ multiple values.py
@@ -4,13 +4,13 @@ for i in range (n):
   t=int(input("Enter the value of wind temperature :"))
   h=int(input("Enter the value of humidity :"))
 
-W=0.5*t**2-0.2*h+0.1*w-15
-print(W)
-if(W>300):
+  W=0.5*t**2-0.2*h+0.1*w-15
+  print(W)
+ if(W>300):
     print("Weather is Sunny")
-elif(200<W<=300):
+ elif(200<W<=300):
     print("Weather is Cloudy")
-elif(100<W<=200):
+ elif(100<W<=200):
         print("Weather is Rainy")
-else:
+ else:
      print("weather is stormy")
